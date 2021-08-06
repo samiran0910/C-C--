f9c7a7824335ba9f672329c306b9e4d72ca3929e
ron0910 <baishya.shamiran2002@gmail.com>
2021-08-06 17:50:28 +0530

    three

---
+++ Learn C++/three.cpp
@@ -0,0 +1,17 @@
+/*  VARIABLES in C++
+    A variable is a container to hold data
+    Variables can be of different types, for example'
+    
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int author = 10;
+    cout<< author;
+    return 0;
+}
